7b1bb1d007199d16db2f6db2e50ebe9c951d26cd
Abir <ak.cr852456@gmail.com>
2021-12-21 09:29:39 +0600

    Java Problem Solving

---
+++ problem.js
@@ -0,0 +1,12 @@
+var num_one = 10;
+var num_two = 5;
+
+var addition = num_one + num_two;
+var substraction = num_one - num_two;
+var division = num_one / num_two;
+var multiply = num_one * num_two;
+
+console.log(addition)
+console.log(substraction)
+console.log(division)
+console.log(multiply)
